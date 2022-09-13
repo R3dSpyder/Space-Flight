01d84af8ec34363e1729e37992d3e06e65f993b3
jss118 <joesmith_guitar@hotmail.co.uk>
2022-09-13 16:05:49 +0100

    add scrolls page, fully functional

--- client/Space-Flight/components/CollectedScrolls.jsx
+++ client/Space-Flight/components/CollectedScrolls.jsx
@@ -2,29 +2,56 @@ import { useContext } from "react";
 import { View, Text, ScrollView, StyleSheet } from "react-native";
 import { planetData } from "../api";
 import { UserContext } from "../context";
-
 const CollectedScrolls = () => {
   const { userInfo } = useContext(UserContext);
   const listOfPlanets = planetData["_3"];
-
-  console.log(userInfo);
-
+  console.log(userInfo.scrolls);
+  const unlocked = index => {
+    if (userInfo.scrolls >= index) {
+      return true;
+    } else {
+      return false;
+    }
+  };
   return (
     <View
       style={{
         alignItems: "center",
         flex: 1,
         justifyContent: "center",
-        // backgroundColor: "black",
+        backgroundColor: "black",
       }}
     >
       <ScrollView>
         {listOfPlanets.map((planet, index) => {
           return (
-            <View style={styles.unlockedScroll} key={index}>
-              <Text>{planet.englishName}</Text>
-              <Text>Discovered by: {planet.discoveredBy}</Text>
-              <Text>Discovery date: {planet.discoveryDate}</Text>
+            <View
+              style={
+                unlocked(index + 1)
+                  ? styles.unlockedScroll
+                  : styles.lockedScroll
+              }
+              key={index}
+            >
+              <Text style={styles.title}>{planet.englishName}</Text>
+              {/* <Text>Discovered by: {planet.discoveredBy}</Text> */}
+              {planet.discoveredBy === "" ? (
+                <Text>Discovered By: Unknown</Text>
+              ) : (
+                <Text>
+                  Discovered By:
+                  {planet.discoveredBy}
+                </Text>
+              )}
+              {planet.discoveryDate === "" ? (
+                <Text>Discovery Date: Unknown</Text>
+              ) : (
+                <Text>
+                  Discovery Date:
+                  {planet.discoveryDate}
+                </Text>
+              )}
+
               {planet.moons === null ? (
                 <Text>Number of moons: Unknown</Text>
               ) : (
@@ -42,7 +69,6 @@ const CollectedScrolls = () => {
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   unlockedScroll: {
     flex: 1,
@@ -50,11 +76,25 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderWidth: 1,
     borderColor: "black",
-    borderRadius: 3,
+    borderRadius: 10,
     margin: 5,
     padding: 5,
     backgroundColor: "white",
   },
+  lockedScroll: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+    margin: 5,
+    padding: 5,
+    backgroundColor: "black",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
 });
-
 export default CollectedScrolls;
